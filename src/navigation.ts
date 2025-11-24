900168521d11fa660cd2bd634921c8bd75be0f95
tranbox6 <tranbox6@gmail.com>
2025-11-24 09:53:56 +0300

    Add streaming schedule with countdown

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,6 +3,10 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
+      text: 'جدول البث',
+      href: getPermalink('/schedule'),
+    },
+    {
       text: 'Homes',
       links: [
         {
